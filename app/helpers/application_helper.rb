de63085319e8ead395993b30616295c515a06388
Ederson de Lima <edersondelima@gmail.com>
2013-05-30 03:02:53 -0300

    Adicionado novo css temporario, aplicado a internacionalizacao e criano dois novos metodos nos application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def error_tag(model, attribute)
+    if(model.errors.has_key? attribute)
+      content_tag :div, model.errors[attribute].first, :class => 'error_message'
+    end
+  end
+
+  def show_errors_tag(model)
+
+    if(model.errors.any?)
+      html = "<div id='error_explanation'>"
+      html += "<h2>#{model.errors.count} #{t('errors.error').pluralize} #{t('errors.message')} </h2>"
+      html += "<ul>"
+
+      model.errors.full_messages.each do |err|
+        html += "<li>#{err}</li>"
+      end
+
+      html += "</ul>"
+      html += "</div>"
+      html.html_safe
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Mybeers::Application.routes.draw do
 
+
   resources :users
   resources :beers
+
+
   root :to => "home#index"
 end
